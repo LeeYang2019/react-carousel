5f6a2a47592051b6546571f8eec8e1f016a57089
leeyang2019 <nyang@madisoncollege.edu>
2020-06-22 12:17:58 -0400

    added featured

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './resources/styles.css';
 import Header from './components/header_footer/Header';
 import Footer from './components/header_footer/Footer';
+import Featured from './components/featured/Featured';
 
 function App() {
   return (
     <div className="App" style={{ height: '1000px', backgroundColor: 'green' }}>
       <Header />
+      <Featured />
       <Footer />
     </div>
   );
