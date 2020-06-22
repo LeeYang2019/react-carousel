04d67ce04ec19b5bccd791c47e9d5d74df397faa
leeyang2019 <nyang@madisoncollege.edu>
2020-06-22 08:14:57 -0400

    upgraded yarn dependencies

---
+++ src/components/SideDrawer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+
+const SideDrawer = ({ open, onClose }) => {
+  return (
+    <Drawer anchor="right" open={open} onClose={() => onClose}>
+      <List component="nav">
+        <ListItem button onClick={() => console.log('featured')}>
+          EventStartsEnd
+        </ListItem>
+        <ListItem button onClick={() => console.log('info')}>
+          VenuFO
+        </ListItem>
+        <ListItem button onClick={() => console.log('highlights')}>
+          Highlights
+        </ListItem>
+        <ListItem button onClick={() => console.log('pricing')}>
+          Pricing
+        </ListItem>
+        <ListItem button onClick={() => console.log('location')}>
+          Location
+        </ListItem>
+      </List>
+    </Drawer>
+  );
+};
+
+export default SideDrawer;
