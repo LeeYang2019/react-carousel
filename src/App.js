f0d58ac66c1211f022c7f6f4cb9081cbad54b026
leeyang2019 <nyang@madisoncollege.edu>
2020-06-29 12:40:52 -0400

    feat: created scrollbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Element } from 'react-scroll';
 import './resources/styles.css';
 import Header from './components/header_footer/Header';
 import Featured from './components/featured/Featured';
@@ -6,16 +7,32 @@ import VenueInfo from './components/venueInfo/VenueInfo';
 import Highlights from './components/highlights/Highlights';
 import Pricing from './components/pricing/Pricing';
 import Location from './components/location/Location';
+import Footer from './components/header_footer/Footer';
 
 function App() {
   return (
     <div className="App" style={{ height: '1000px', backgroundColor: 'blue' }}>
-      <Header />
-      <Featured />
-      <VenueInfo />
-      <Highlights />
-      <Pricing />
-      <Location />
+      <Element>
+        <Header />
+      </Element>
+      <Element name="featured">
+        <Featured />
+      </Element>
+      <Element name="info">
+        <VenueInfo />
+      </Element>
+      <Element name="highlights">
+        <Highlights />
+      </Element>
+      <Element name="pricing">
+        <Pricing />
+      </Element>
+      <Element name="location">
+        <Location />
+      </Element>
+      <Element>
+        <Footer />
+      </Element>
     </div>
   );
 }
--- src/components/SideDrawer.js
+++ src/components/SideDrawer.js
@@ -1,25 +1,42 @@
 import React from 'react';
+import { scroller } from 'react-scroll';
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 
 const SideDrawer = ({ open, onClose }) => {
+
+  const scrollToElement = (element) => {
+    scroller.scrollTo(element, {
+      duration: 1500,
+      delay: 100,
+      smooth: true,
+      offset: -150
+    });
+    onClose(false);
+  }
+
   return (
-    <Drawer anchor="right" open={open} onClose={() => onClose}>
+    <Drawer
+      anchor="right"
+      open={open}
+      onClose={() => onClose(false)
+      }>
+
       <List component="nav">
-        <ListItem button onClick={() => console.log('featured')}>
-          EventStartsEnd
+        <ListItem button onClick={() => scrollToElement('featured')}>
+          Event Starts In
         </ListItem>
-        <ListItem button onClick={() => console.log('info')}>
-          VenuFO
+        <ListItem button onClick={() => scrollToElement('info')}>
+          Venue Info
         </ListItem>
-        <ListItem button onClick={() => console.log('highlights')}>
+        <ListItem button onClick={() => scrollToElement('highlights')}>
           Highlights
         </ListItem>
-        <ListItem button onClick={() => console.log('pricing')}>
+        <ListItem button onClick={() => scrollToElement('pricing')}>
           Pricing
         </ListItem>
-        <ListItem button onClick={() => console.log('location')}>
+        <ListItem button onClick={() => scrollToElement('location')}>
           Location
         </ListItem>
       </List>
